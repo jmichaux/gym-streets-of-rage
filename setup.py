0de3c8ccd9706f89f4a65df3188a6740b305aa3b
Jonathan Michaux <jmichaux@ttic.edu>
2017-12-28 10:37:35 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
 
-setup(name='gym_sor',
+setup(name='gym_streets_of_rage',
       version='0.0.1',
+      description='Gym User Env - Every level of the original Streets of Rage triology',
+      url='https://github.com/jmichaux/gym-streets-of-rage',
+      author='Jonathan Michaux',
+      author_email='jmichaux@ttic.edu', 
       install_requires=['gym>=0.2.3',
                         'rle_python_interface>=1.1.5', 'PyOpenGL']
 )
